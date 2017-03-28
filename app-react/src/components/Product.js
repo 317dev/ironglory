e19cd255b2d793122475377f792821da9f74818e
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-28 18:14:30 -0400

    Created Product component

--- app-react/src/components/Product.js
+++ app-react/src/components/Product.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-// import product.css
+import styles from '../public/css/product.css';
 
-class Product extends Component {
+class Product extends Component.React {
   render() {
-    return (
-      <h1>Hello Product</h1>
-    );
+    return <div>
+    <div className="row">
+    <div className="col-sm-6 col-md-4">
+      <div className="thumbnail">
+        <img src="https://unsplash.it/200/?blur" alt="{this.props.description}" />
+        <div className="caption">
+          <h3>{this.props.title}</h3>
+          <div className="row">
+              <div className="col-sm-6">
+                  <p>{this.props.quantity}</p>
+              </div>
+              <div className="col-sm-6 pull-right">
+                  <p>{this.props.price}</p>
+              </div>
+              <a href="#####" className="btn btn-success pull-right" role="button">+</a>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   }
 }
 
-export default Product;
+export default Product
