e6449f8bbf0e93427fa4555680f534d03f540fd6
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-31 12:56:27 -0400

    Rendered patches on homepage

--- app-react/src/components/Product.js
+++ app-react/src/components/Product.js
@@ -5,21 +5,28 @@ class Product extends React.Component {
     // componentWillMount() {
     //     this.props.getPatches(this.props.params.id)
     // }
-  render() {
 
+        getPatches() {
+        fetch('/api/patches')
+        .then(res => res.json())
+        .then(res => this.setState({patches: res, originalPatches: res}))
+    }
+
+  render() {
+console.log(this.props.patch)
     return <div>
       <div className="row">
       <div className="col-sm-6 col-md-4">
         <div className="thumbnail">
-          <img src="https://unsplash.it/200/?blur" alt="{this.props.description}" />
+          <img src={this.props.patch.image} alt={this.props.patch.description} />
           <div className="caption">
             <h3>{this.props.name}</h3>
             <div className="row">
                 <div className="col-sm-6">
-                    <p>{this.props.quantity}</p>
+                    <p>{this.props.patch.quantity}</p>
                 </div>
                 <div className="col-sm-6 pull-right">
-                    <p>{this.props.price}</p>
+                    <p>{this.props.patch.price}</p>
                 </div>
                 <a href="#####" className="btn btn-success pull-right" role="button">+</a>
             </div>
